e403a1c9b49aae90cc8f807dbb0553fab15b60f2
soyeonvv <thdusdlvv@naver.com>
2021-09-29 00:23:38 +0900

    CBV로 블로그 페이지 만들기

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [ # 서버IP/blog
-    path('<int:pk>/', views.single_post_page),
-    path('', views.index),   
+#    path('<int:pk>/', views.post_detail),
+#    path('', views.post_list),   
+
+    path('<int:pk>/', views.PostDetail.as_view()),
+    path('', views.PostList.as_view()),
 ]
