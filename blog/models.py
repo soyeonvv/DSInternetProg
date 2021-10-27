3bceb4affc3e1ce70d96ab00a069144bed781dbd
soyeonvv <thdusdlvv@naver.com>
2021-10-27 14:01:10 +0900

    포스트 목록/상세 페이지에 카테고리 기능 추가하기

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,15 @@ from django.contrib.auth.models import User
 import os
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Categories'
 
 class Post(models.Model):
     title = models.CharField(max_length=30)
@@ -14,8 +23,9 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    #author
     author = models.ForeignKey(User,null=True,on_delete=models.SET_NULL)
+
+    category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
 
     def __str__(self):
         return f'[{self.pk}]{self.title} :: {self.author}'
