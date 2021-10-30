f0c6a34f7829820c7d7914cced67ce914eb5b663
soyeonvv <thdusdlvv@naver.com>
2021-10-31 00:42:12 +0900

    카테고리 페이지 만들기

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [ # 서버IP/blog
 #    path('<int:pk>/', views.post_detail),
 #    path('', views.post_list),   
 
+    path('category/<slug>', views.category_page),
     path('<int:pk>/', views.PostDetail.as_view()),
     path('', views.PostList.as_view()),
 ]
