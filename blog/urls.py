ff5b2b9a2b11e1531ab929bacbb6da0381e01414
soyeonvv <thdusdlvv@naver.com>
2021-10-31 01:32:16 +0900

    포스트 목록/상세 페이지에 태그 기능 추가 및 테그페이지 만들기

--- blog/urls.py
+++ blog/urls.py
@@ -5,7 +5,8 @@ urlpatterns = [ # 서버IP/blog
 #    path('<int:pk>/', views.post_detail),
 #    path('', views.post_list),   
 
-    path('category/<slug>', views.category_page),
+    path('tag/<str:slug>', views.tag_page), #서버IP/blog/tag/slug
+    path('category/<slug>', views.category_page),   # 서버IP/blog/category/slug
     path('<int:pk>/', views.PostDetail.as_view()),
     path('', views.PostList.as_view()),
 ]
