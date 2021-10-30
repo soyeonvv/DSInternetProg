68846678dcaf7d9a62c8d0af4787f09404d45aa1
soyeonvv <thdusdlvv@naver.com>
2021-10-31 02:06:25 +0900

    로그인한 사용자에게만 포스트 작성 페이지 열어주기

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [ # 서버IP/blog
 #    path('<int:pk>/', views.post_detail),
 #    path('', views.post_list),   
 
+    path('create_post/', views.PostCreate.as_view()),
     path('tag/<str:slug>', views.tag_page), #서버IP/blog/tag/slug
     path('category/<slug>', views.category_page),   # 서버IP/blog/category/slug
     path('<int:pk>/', views.PostDetail.as_view()),
