106e39b3f6142062068900ec5828cbf15e6dbba9
soyeonvv <thdusdlvv@naver.com>
2021-11-06 23:35:45 +0900

    포스트 작성 페이지 접근 권한 설정하기

--- blog/tests.py
+++ blog/tests.py
@@ -9,6 +9,8 @@ class TestView(TestCase):
         self.client = Client()
 
         self.user_james = User.objects.create_user(username='James', password='somepassword')
+        self.user_james.is_staff = True
+        self.user_james.save()
         self.user_trump= User.objects.create_user(username='Trump', password='somepassword')
 
         self.category_programming= Category.objects.create(name='programming', slug='programming')
@@ -104,7 +106,12 @@ class TestView(TestCase):
         self.client.login(username='Trump', password='somepassword')
         response = self.client.get('/blog/create_post/')
         # 정상적으로 페이지가 로드
+        self.assertNotEqual(response.status_code, 200)
+
+        self.client.login(username='James', password='somepassword')
+        response = self.client.get('/blog/create_post/')
         self.assertEqual(response.status_code, 200)
+
         # 페이지 타이틀 'Blog'
         soup = BeautifulSoup(response.content, 'html.parser')
         self.assertEqual(soup.title.text, 'Create Post - Blog')
@@ -118,7 +125,7 @@ class TestView(TestCase):
             })
         last_post = Post.objects.last()
         self.assertEqual(last_post.title, "Post form 만들기")
-        self.assertEqual(last_post.author.username, 'Trump')
+        self.assertEqual(last_post.author.username, 'James')
 
     def test_post_list(self):
         self.assertEqual(Post.objects.count(), 3)
