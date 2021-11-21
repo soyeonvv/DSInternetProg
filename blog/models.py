2f23413e6eacfe1bcb68cf1848c7df74e099c761
soyeonvv <thdusdlvv@naver.com>
2021-11-22 01:32:31 +0900

    사용자 아바타 보여주기

--- blog/models.py
+++ blog/models.py
@@ -70,3 +70,9 @@ class Comment(models.Model):
 
     def get_absolute_url(self):
         return f'{self.post.get_absolute_url()}#comment-{self.pk}'
+
+    def get_avatar_url(self):
+        if self.author.socialaccount_set.exists() :
+            return self.author.socialaccount_set.first().get_avatar_url()
+        else :
+            return 'https://doitdjango.com/avatar/id/372/b84a6d50c48eac54/svg/{self.author.email}/'
