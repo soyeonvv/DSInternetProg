71a21569552d46930fae32794ed2266fb61439db
soyeonvv <thdusdlvv@naver.com>
2021-11-28 04:33:59 +0900

    대문 페이지 수정하기

--- blog/models.py
+++ blog/models.py
@@ -59,6 +59,12 @@ class Post(models.Model):
     def get_content_markdown(self):
         return markdown(self.content)
 
+    def get_avatar_url(self):
+        if self.author.socialaccount_set.exists() :
+            return self.author.socialaccount_set.first().get_avatar_url()
+        else :
+            return 'https://doitdjango.com/avatar/id/372/b84a6d50c48eac54/svg/{self.author.email}/'
+
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
