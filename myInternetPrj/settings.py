0670fcd4764b8c037218b6c6f1455a55671ec33e
soyeonvv <thdusdlvv@naver.com>
2021-11-29 00:53:39 +0900

    도커에 PostgresQL 설정 추가하기

--- myInternetPrj/settings.py
+++ myInternetPrj/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-_hoyy17_nebqn21b$co4%ehfojtvjugyoof7ouvg*@ec5_bs6+')
+SECRET_KEY = os.environ.get('SECRET_KEY','django-insecure-_hoyy17_nebqn21b$co4%ehfojtvjugyoof7ouvg*@ec5_bs6+')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get('DEBUG', 1))
@@ -88,8 +88,12 @@ WSGI_APPLICATION = 'myInternetPrj.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE' : os.environ.get("SQL_ENGINE", 'django.db.backends.sqlite3'),
+        'NAME' : os.environ.get("SQL_DATABASE", os.path.join(BASE_DIR,'db.sqlite3')),
+        'USER' : os.environ.get("SQL_USER", 'user'),
+        'PASSWORD' : os.environ.get("SQL_PASSWORD", 'password'),
+        'HOST' : os.environ.get("SQL_HOST", 'localhost'),
+        'PORT' : os.environ.get("SQL_PORT", '5432')
     }
 }
 
